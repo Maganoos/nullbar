ef5fd0fc1c6e808bce3c7f61eb266d45d3071987
Magnus <magkari@pm.me>
2025-09-24 18:54:21 +0000

    fix: change mixin type to fake not having any waypoints, should be more compatible

--- src/main/java/eu/magkari/mc/nullbar/mixin/MixinInGameHud.java
+++ src/main/java/eu/magkari/mc/nullbar/mixin/MixinInGameHud.java
@@ -5,18 +5,15 @@ import static eu.magkari.mc.nullbar.NullBar.show;
 import net.minecraft.client.gui.hud.InGameHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
-	@Redirect(
-			method = "getCurrentBarType",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/client/gui/hud/InGameHud;shouldShowExperienceBar()Z"
-			)
-	)
-	private boolean showExperienceBarOverride(InGameHud instance) {
-        return !show;
+    @ModifyVariable(
+            method = "getCurrentBarType",
+            at = @At(value = "STORE"),
+            ordinal = 0)
+    private boolean nullbar$showLocator(boolean v) {
+        return show && v;
     }
 }
